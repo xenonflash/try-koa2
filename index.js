204e91420ea6055aabfeef8baf0fe531a5141f7c
yankechao <yankechao@cloudin.cn>
2017-06-26 15:45:19 +0800

    使用router

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
- const Koa = require('koa');
- const app = new Koa();
+const Koa = require('koa');
+const app = new Koa();
+const router = require('koa-router')();
 var mid_processTime = require('./middleware/logTime');
 var mid_logRequest = require('./middleware/logRequest');
 
  app.use(mid_logRequest);
- app.use(mid_processTime);
- app.use(async (ctx, next) => {
-   await next();
-   ctx.response.type = "text/html";
-   ctx.response.body = '<h1>hello koa</h1>';
- });
+// add url-route:
+router.get('/hello/:name', async (ctx, next) => {
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+});
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = '<h1>Index</h1>';
+});
+
+// add router middleware:
+app.use(router.routes());
+app.use(mid_processTime);
+
+
+
  app.listen(3000);
  console.log('server started at 3000...');
